c1beb632c1bf8395df360521b72dc4ca325c226d
Ajut Dangol <ajud10gol@gmail.com>
2024-08-18 09:14:23 +0545

    render

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  return (
-    <>
-     <h1>helo jata tetai</h1>
-    </>
-  )
+  //render method
+  // array of elements
+  // return [<h1>hello world</h1>,
+  //    <p>this is new </p>];
+  
+  // div
+    //  return (
+    //   <div>
+    //     <h1>hello</h1>
+    //     <p>Lorem ipsum dolor sit.</p>
+    //   </div>
+    //  )
+    
+    //  return (
+    //   <React.Fragment>
+    //     <h1>hello</h1>
+    //     <p>Lorem ipsum dolor sit.</p>
+    //   </React.Fragment>
+    //  )
+
+    // 
+    const styling ={
+      color: "Green",
+      font:"4rem",
+    }
+     return (
+      <>
+        {/* inline css */}
+        <h1 style={{color:"green",font:"2rem"}}>hello</h1>
+        {/* variable */}
+        <p style={styling}>Lorem ipsum dolor sit.</p>
+        <span className="text-red-500">Lorem ipsum dolor sit amet.</span>
+      </>
+     );
+
+    
+  
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// Render() display only one time
+// React.strict mode is used for printing two time
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
